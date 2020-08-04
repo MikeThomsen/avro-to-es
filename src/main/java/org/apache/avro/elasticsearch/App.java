04b4b4738c15e3106d243c13da8a2123ae5e5c6b
Mike Thomsen <mikerthomsen@gmail.com>
2020-08-04 19:38:51 -0400

    Added jackson writer.

--- src/main/java/org/apache/avro/elasticsearch/App.java
+++ src/main/java/org/apache/avro/elasticsearch/App.java
@@ -1,5 +1,6 @@
 package org.apache.avro.elasticsearch;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.Schema;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -9,8 +10,16 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class App {
+    public static final String KEY_TYPE = "es_type";
+    public static final String KEY_FIELDS = "es_fields";
+
     private static CommandLine parseArguments(String[] args) throws ParseException {
         Options options = new Options();
 
@@ -31,6 +40,18 @@ public class App {
         return parser.parse(options, args);
     }
 
+    public static void convert(Map<String, Object> map, List<Schema.Field> fields) {
+
+    }
+
+    public static void writeJson(Map<String, Object> map, String output) throws IOException  {
+        String json = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(map);
+        FileWriter writer = new FileWriter(output);
+        writer.write(json);
+        writer.close();
+        System.out.println(String.format("Wrote schema to %s", output));
+    }
+
     public static void main(String[] args) throws Exception {
         CommandLine arguments = parseArguments(args);
 
@@ -39,7 +60,16 @@ public class App {
         String mapping = arguments.getOptionValue("mapping");
 
         Schema parsedSchema = new Schema.Parser().parse(new FileInputStream(schema));
-        parsedSchema.getFields().forEach(field -> System.out.println(field.getObjectProp("es_type")));
-        parsedSchema.getFields().forEach(field -> System.out.println(field.getObjectProp("es_fields")));
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> mappings = new HashMap<>();
+        Map<String, Object> thisMap = new HashMap<>();
+        Map<String, Object> properties = new HashMap<>();
+        map.put("mappings", mappings);
+        mappings.put(mapping, thisMap);
+        thisMap.put("properties", properties);
+
+        convert(properties, parsedSchema.getFields());
+
+        writeJson(map, output);
     }
 }
